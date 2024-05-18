53adb44cef930636050660cb91267fbd3caf9933
Tomás Manoukian <tomanoukian@gmail.com>
2024-05-18 14:24:29 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var edad = 37;
+if (edad >= 38) {
+    console.log("Tengo 38 años");
+}
+else {
+    console.log("¿De qué color es la arcilla en Tricao?");
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+const edad = 37;
+
+if(edad>=38){
+    console.log("Tengo 38 años");
+}else{
+    console.log("¿De qué color es la arcilla en Tricao?")
+}
